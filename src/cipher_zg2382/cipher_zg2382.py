46e4f2ef5de0ce9b288e77c04d6873fbd1137152
Zhaokailu Gu <gzklcece@gmail.com>
2021-11-09 00:11:11 -0500

    Added cipher function

---
+++ src/cipher_zg2382/cipher_zg2382.py
@@ -0,0 +1,42 @@
+def cipher(text, shift, encrypt=True):
+    """
+    encrypting or decrypting a text with Caesar cipher.
+    It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet
+
+    Parameters
+    ----------
+    text : string
+      text waiting to be encoded.
+    shift: int
+      shift of positions.
+    encrypt: boolean
+       If True, shift down the alphabet(encrypt);
+       If False, shift up the alphabet(decrypt)
+
+    Returns
+    -------
+    string
+       text output after encrypt or decrypt.
+
+    Examples1 encrypting
+    --------
+    >>> from cipher_zg2382 import cipher
+    >>> cipher('D', 3, True)
+    'A'
+    Examples2 encrypting
+    --------
+    >>> from cipher_zg2382 import cipher
+    >>> cipher('A', 3, False)
+    'D'
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
